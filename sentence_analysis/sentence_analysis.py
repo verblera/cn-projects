6605ceaf30766ba2255473a3b8c9156cfc70030c
Lera <werblera@gmail.com>
2021-01-28 07:21:11 +0000

    adding 'total'

---
+++ sentence_analysis/sentence_analysis.py
@@ -0,0 +1,16 @@
+""""ff"""
+
+sentence = input('Please, type in a sentence of your choice: ')
+
+dict_ = {'lower': 0, 'upper': 0, 'punct': 0, 'total': 0}
+sentence = sentence.replace(' ','')
+for char in sentence:
+    if char.islower():
+        dict_['lower'] += 1
+    elif char.isupper():
+        dict_['upper'] += 1
+    else:
+        dict_['punct'] += 1
+dict_['total'] = dict_['upper'] + dict_['lower'] + dict_['punct']
+print(f"Lower case letters: {dict_['lower']}.\nUpper case letters: {dict_['upper']}.\n"
+      f"Punctuation: {dict_['punct']}\nTotal: {dict_['total']}")
